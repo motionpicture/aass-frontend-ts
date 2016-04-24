44254fbc13cf93bc7f0c840dcd7124590c633e3b
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-04-24 18:16:41 +0900

    AASS-12 デプロイスクリプト追加。

--- bin/deploy.cmd
+++ bin/deploy.cmd
@@ -1,13 +1,19 @@
 set SCM_COMMAND_IDLE_TIMEOUT 9999
 xcopy /E /Y %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET%
+
 cd %DEPLOYMENT_TARGET%
+
 npm install -g typescript
+
 npm install -g typings
+
 npm install
 typings install
+
 cd ./public
 npm install
 typings install
+
 cd ../
 tsc -p ./
 tsc -p ./public/
