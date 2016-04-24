d77b77b632e0be1f8a53c9b25c6f1009b9047c65
Tetsu Yamazaki <yamazaki@motionpicture.jp>
2016-04-24 18:20:27 +0900

    AASS-12 デプロイスクリプト追加。

--- bin/deploy.cmd
+++ bin/deploy.cmd
@@ -3,9 +3,9 @@ xcopy /E /Y %DEPLOYMENT_SOURCE% %DEPLOYMENT_TARGET%
 
 cd %DEPLOYMENT_TARGET%
 
-npm install -g typescript
+rem npm install -g typescript
 
-npm install -g typings
+rem npm install -g typings
 
 npm install
 typings install
