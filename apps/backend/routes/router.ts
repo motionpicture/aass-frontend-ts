b09b0855d660181af2e43ee4312d719617cd0af5
akito hataguchi <hataguchi@motionpicture.jp>
2016-05-18 11:22:28 +0900

    AASS-11 router部分余計なインスタンス化削除

--- apps/backend/routes/router.ts
+++ apps/backend/routes/router.ts
@@ -1,3 +1,5 @@
+/// <reference path="../../../typings/main.d.ts" />
+
 import Router = require('named-routes');
 
 import Application from '../controllers/Application';
@@ -10,28 +12,25 @@ export default function router(app: any) {
     router.extendExpress(app);
     router.registerAppHelpers(app);
 
-    let application = new Application();
-    let auth = new Auth();
-    let event = new Event();
-    let media = new Media();
+    
 
     app.get('/admin', 'home', function(req, res, next) {
         res.redirect('/admin/events');
     });
 
-    app.all('/admin/login', 'login', (req, res, next) => {auth.login(req, res, next)});
-    app.post('/admin/logout', 'logout', (req, res, next) => {auth.logout(req, res, next)});
+    app.all('/admin/login', 'login', (req, res, next) => {(new Auth()).login(req, res, next)});
+    app.post('/admin/logout', 'logout', (req, res, next) => {(new Auth()).logout(req, res, next)});
 
-    app.post('/admin/application/:id/accept', 'application.accept', (req, res, next) => {application.accept(req, res, next)});
-    app.post('/admin/application/:id/reject', 'application.reject', (req, res, next) => {application.reject(req, res, next)});
+    app.post('/admin/application/:id/accept', 'application.accept', (req, res, next) => {(new Application()).accept(req, res, next)});
+    app.post('/admin/application/:id/reject', 'application.reject', (req, res, next) => {(new Application()).reject(req, res, next)});
 
-    app.get('/admin/events', 'events', (req, res, next) => {event.list(req, res, next)});
-    app.all('/admin/event/new', 'event.create', (req, res, next) => {event.create(req, res, next)});
-    app.all('/admin/event/validate', 'event.validate', (req, res, next) => {event.validate(req, res, next)});
-    app.all('/admin/event/:id/edit', 'event.update', (req, res, next) => {event.update(req, res, next)});
-    app.get('/admin/event/:id/medias', 'event.medias', (req, res, next) => {event.medias(req, res, next)});
+    app.get('/admin/events', 'events', (req, res, next) => {(new Event()).list(req, res, next)});
+    app.all('/admin/event/new', 'event.create', (req, res, next) => {(new Event()).create(req, res, next)});
+    app.all('/admin/event/validate', 'event.validate', (req, res, next) => {(new Event()).validate(req, res, next)});
+    app.all('/admin/event/:id/edit', 'event.update', (req, res, next) => {(new Event()).update(req, res, next)});
+    app.get('/admin/event/:id/medias', 'event.medias', (req, res, next) => {(new Event()).medias(req, res, next)});
 
-    app.get('/admin/media/:id/download', 'media.download', (req, res, next) => {media.download(req, res, next)});
-    app.get('/admin/media/:id/download/jpeg2000', 'media.downloadJpeg2000', (req, res, next) => {media.downloadJpeg2000(req, res, next)});
-    app.post('/admin/media/:id/encode2jpeg2000', 'media.encode2jpeg2000', (req, res, next) => {media.encode2jpeg2000(req, res, next)});
+    app.get('/admin/media/:id/download', 'media.download', (req, res, next) => {(new Media()).download(req, res, next)});
+    app.get('/admin/media/:id/download/jpeg2000', 'media.downloadJpeg2000', (req, res, next) => {(new Media()).downloadJpeg2000(req, res, next)});
+    app.post('/admin/media/:id/encode2jpeg2000', 'media.encode2jpeg2000', (req, res, next) => {(new Media()).encode2jpeg2000(req, res, next)});
 }
--- apps/frontend/routes/router.ts
+++ apps/frontend/routes/router.ts
@@ -9,26 +9,23 @@ export default function router(app: any) {
     router.extendExpress(app);
     router.registerAppHelpers(app);
 
-    let auth = new Auth();
-    let media = new Media();
-    let application = new Application();
-
+    
     app.get('/', 'home', function(req, res, next) {
         res.redirect('/medias');
     });
 
-    app.all('/login', 'login', (req, res, next) => {auth.login(req, res, next)});
-    app.post('/logout', 'logout', (req, res, next) => {auth.logout(req, res, next)});
+    app.all('/login', 'login', (req, res, next) => {(new Auth()).login(req, res, next)});
+    app.post('/logout', 'logout', (req, res, next) => {(new Auth()).logout(req, res, next)});
 
-    app.get('/medias', 'medias', (req, res, next) => {media.list(req, res, next)});
-    app.all('/media/new', 'media.create', (req, res, next) => {media.create(req, res, next)});
-    app.post('/media/createAsset', 'media.createAsset', (req, res, next) => {media.createAsset(req, res, next)});
-    app.post('/media/appendFile', 'media.appendFile', (req, res, next) => {media.appendFile(req, res, next)});
-    app.post('/media/commitFile', 'media.commitFile', (req, res, next) => {media.commitFile(req, res, next)});
-    app.all('/media/:id/edit', 'media.update', (req, res, next) => {media.update(req, res, next)});
-    app.get('/media/:id/download', 'media.download', (req, res, next) => {media.download(req, res, next)});
-    app.post('/media/:id/delete', 'media.delete', (req, res, next) => {media.delete(req, res, next)});
-    app.post('/media/:id/apply', 'media.apply', (req, res, next) => {media.apply(req, res, next)});
+    app.get('/medias', 'medias', (req, res, next) => {(new Media()).list(req, res, next)});
+    app.all('/media/new', 'media.create', (req, res, next) => {(new Media()).create(req, res, next)});
+    app.post('/media/createAsset', 'media.createAsset', (req, res, next) => {(new Media()).createAsset(req, res, next)});
+    app.post('/media/appendFile', 'media.appendFile', (req, res, next) => {(new Media()).appendFile(req, res, next)});
+    app.post('/media/commitFile', 'media.commitFile', (req, res, next) => {(new Media()).commitFile(req, res, next)});
+    app.all('/media/:id/edit', 'media.update', (req, res, next) => {(new Media()).update(req, res, next)});
+    app.get('/media/:id/download', 'media.download', (req, res, next) => {(new Media()).download(req, res, next)});
+    app.post('/media/:id/delete', 'media.delete', (req, res, next) => {(new Media()).delete(req, res, next)});
+    app.post('/media/:id/apply', 'media.apply', (req, res, next) => {(new Media()).apply(req, res, next)});
 
-    app.post('/application/:id/reset', 'application.reset', (req, res, next) => {application.reset(req, res, next)});
+    app.post('/application/:id/reset', 'application.reset', (req, res, next) => {(new Application()).reset(req, res, next)});
 }
